1c07a1bd468702cb5088ea23370fb6e9708d2ed3
Ganesh <ganeshgarje716@gmail.com>
2026-07-19 22:31:26 +0530

    Create Student Class

---
+++ Spring-JWT-Security-Student/src/main/java/com/ganesh/entity/Student.java
@@ -0,0 +1,14 @@
+package com.ganesh.entity;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Entity
+public class Student {
+
+}
