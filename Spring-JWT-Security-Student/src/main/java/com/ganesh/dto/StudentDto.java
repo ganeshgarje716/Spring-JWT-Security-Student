f055d5ac10b5e65a5dee68f6228b9f334beba605
Ganesh <ganeshgarje716@gmail.com>
2026-07-21 21:27:30 +0530

    Create Variables And Add Validations

--- Spring-JWT-Security-Student/src/main/java/com/ganesh/dto/StudentDto.java
+++ Spring-JWT-Security-Student/src/main/java/com/ganesh/dto/StudentDto.java
@@ -1,8 +1,31 @@
 package com.ganesh.dto;
 
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 
 @Data
 public class StudentDto {
+	
+	
+	
+	private Integer id;
+	
+    @NotNull(message = "Name Is Require")
+	private String name;
+	
+    @NotNull(message = "Address Is Require")
+	private String address;
+	
+    @NotNull(message = "Age Is Require")
+	private Integer age;
+	
+    @NotNull(message = "Email Is Require")
+    @Column(unique = true)
+	private String email;
+	
+    @NotNull(message = "Mobile No Is Require")
+    @Column(unique = true)
+	private String mobileNo;
 
 }
